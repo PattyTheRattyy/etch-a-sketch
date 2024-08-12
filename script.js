65a7c6b99cc606074031cd7c36588f09aa738d46
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-11 21:36:13 -0400

    Add random color when hovering over a div

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
 const container = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
+    div.classList.add("square");
     container.appendChild(div);
+
+    Math.random();
 }
+
+const squareDiv = document.querySelector("div");
+squareDiv.addEventListener("mouseover", () => {
+    console.log("test");
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let squareColor = `rgb(${r},${g},${b})`;
+    squareDiv.style.backgroundColor = squareColor;
+});
