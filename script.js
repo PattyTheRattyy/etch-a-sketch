3e81bb7dd4098feb1316a78443a12c74329cb172
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-11 22:44:06 -0400

    Add functionality to change grid size
    Got resizing to work and fixed a bug where the color feature stopped working after resizing. Next I have to get the container to resize with the amount of squares in the grid. As well, I have to get the square divs to resize and take up additional space.

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    container.appendChild(div);
+let squareDivs = document.querySelectorAll(".square");
 
-    Math.random();
+
+let input = 16
+setGridSize(16);
+
+console.log(input);
+const inputBtn = document.querySelector("#inputBtn");
+inputBtn.addEventListener("click", () => {
+    input = parseInt(prompt("Choose a number from 1-100", "16"));
+    setGridSize(input);
+});
+
+
+function resetGrid() {
+    container.replaceChildren();
 }
 
-const squareDivs = document.querySelectorAll(".square");
-squareDivs.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-        console.log("test");
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        let squareColor = `rgb(${r},${g},${b})`;
-        item.style.backgroundColor = squareColor;
+
+function setGridSize(gridSize) {
+
+    resetGrid();
+
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        container.appendChild(div);
+    }
+    
+    addColor();
+}
+
+function addColor() {
+    squareDivs = document.querySelectorAll(".square");
+    squareDivs.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            console.log("test");
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            let squareColor = `rgb(${r},${g},${b})`;
+            item.style.backgroundColor = squareColor;
+        });
     });
-});
+}
